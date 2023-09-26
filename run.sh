4225dda57e581c3368a300f79f1c8705ab8152eb
Luis Aguilar Rojas <luis.rojas@suse.com>
2023-09-25 21:45:51 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export DRONE_GITHUB_CLIENT_ID=
-export DRONE_GITHUB_CLIENT_SECRET=
-export DRONE_GITHUB_ADMIN=
-export DRONE_SERVER_HOST=
+export DRONE_GITHUB_CLIENT_ID=55d304180fa091c3215c
+export DRONE_GITHUB_CLIENT_SECRET=5a44e370162bc6bf1b6ab4c751ae9d9c7bce8ba5
+export DRONE_GITHUB_ADMIN=luisrojas2
+export DRONE_SERVER_HOST=0149-181-174-105-146.ngrok-free.app
 
 export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
